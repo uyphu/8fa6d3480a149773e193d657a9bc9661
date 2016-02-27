511113ac562f41db5d600605f45e1b270018f51a
uyphu <uyphu@yahoo.com>
2016-02-28 00:16:35 +0700

    test

--- src/com/ltu/tube/HottubeServlet.java
+++ src/com/ltu/tube/HottubeServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.*;
 public class HottubeServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
+		
 		resp.getWriter().println("Hello, world");
 	}
 }
